da38535c14cddf30b38204ac727519b1f452b9a2
mvpurin <mvpurin@gmail.com>
2025-02-11 14:11:45 +0700

    Add random input

---
+++ src/main/java/input/random/RandomDataProvider.java
@@ -0,0 +1,5 @@
+package input.random;
+
+public interface RandomDataProvider {
+    String generate();
+}
---
+++ src/main/java/input/random/RandomPersonGenerator.java
@@ -0,0 +1,36 @@
+package input.random;
+
+import model.Sex;
+
+import java.util.Random;
+
+public class RandomPersonGenerator implements RandomDataProvider {
+    private static final Random random = new Random();
+    private static final String[] MALE_SURNAMES = {
+            "Ivanov", "Petrov", "Sidorov", "Kuznetsov", "Smirnov", "Popov", "Alekseev",
+            "Belov", "Vasiliev", "Grigoryev", "Denisov", "Yegorov", "Zhukov", "Zaitsev",
+            "Kovalev", "Lebedev", "Morozov", "Nikolaev", "Orlov", "Pavlov", "Romanov",
+            "Safonov", "Tikhonov", "Ulyanov", "Fedorov", "Khoroshev", "Tsvetkov", "Sharikov",
+            "Shcherbakov", "Yakovlev"
+    };
+
+    private static final String[] FEMALE_SURNAMES = {
+            "Ivanova", "Petrova", "Sidorova", "Kuznetsova", "Smirnova", "Popova", "Alekseeva",
+            "Belova", "Vasilieva", "Grigoryeva", "Denisova", "Yegorova", "Zhukova", "Zaitseva",
+            "Kovaleva", "Lebedeva", "Morozova", "Nikolaeva", "Orlova", "Pavlova", "Romanova",
+            "Safonova", "Tikhonova", "Ulyanova", "Fedorova", "Khorosheva", "Tsvetkova",
+            "Sharikova", "Shcherbakova", "Yakovleva"
+    };
+
+    @Override
+    public String generate() {
+        Sex sex = random.nextBoolean() ? Sex.MAN : Sex.WOMEN;
+        int age = random.nextInt(99) + 1;
+
+        String surname = (sex == Sex.MAN)
+                ? MALE_SURNAMES[random.nextInt(MALE_SURNAMES.length)]
+                : FEMALE_SURNAMES[random.nextInt(FEMALE_SURNAMES.length)];
+
+        return String.format("\"%s, %d, %s\"", sex, age, surname);
+    }
+}
